1292c440be54c8f6073ffd024c373befebbcc5ba
Krystian <krystian@trdlnk.com>
2018-05-22 09:01:55 -0500

    renamed to mc

---
+++ mc.py
@@ -0,0 +1,273 @@
+import copy
+class List(list):
+    def __init__(self, args=[]):
+        super(List, self).__init__(args)
+        
+    def map(self,func):
+        l = []
+        for i in self:
+            l.append(func(i))
+        return List(l)
+    
+    def filter(self, func):
+        l = []
+        for i in self:
+            if func(i):
+                l.append(i)
+        return List(l)
+
+    def fold(self, func, starts):
+        v = copy.deepcopy(starts)
+        for i in self:
+            v = func(v,i)
+        return v
+
+    def group_by(self, func):
+        d = {}
+        for i in self:
+            val = func(i)
+            if val not in d:
+                d[val] = List()
+            d[val].append(i)
+        return Dic(d)
+
+    def foreach(self, func):
+        for i in self:
+            func(i)
+
+    def sorted(self, key = None):
+        return List(sorted(self, key=key))
+
+    def to_dict(self):
+        return Dic({i[0]:i[1] for i in self})
+
+    def to_set(self):
+        return Set(self)
+
+    def mk_string(self, sep = ", ", left = "", right = ""):
+        return left + sep.join([str(i) for i in self]) + right
+
+    def __add__(self, other):
+        return List(self + other)
+
+class Set(set):
+    def __init__(self, args={}):
+        super(Set, self).__init__(args)
+
+    def map(self,func):
+        l = set()
+        for i in self:
+            l.add(func(i))
+        return Set(l)
+
+    def filter(self, func):
+        l = set()
+        for i in self:
+            if func(i):
+                l.add(i)
+        return Set(l)
+
+    def to_dict(self):
+        return Dic({i[0]:i[1] for i in self})
+
+    def to_list(self):
+        return List(self)
+
+    def mk_string(self, sep = ", ", left = "", right = ""):
+        return left + sep.join([str(i) for i in self]) + right
+
+class Dic(dict):
+    def __init__(self, args):
+        super(Dic, self).__init__(args)
+    def map_vals(self, func):
+        l = {}
+        for i in self:
+            l[i] = func(i, self[i])
+        return Dic(l)
+    def map_keys(self, func):
+        l = {}
+        for i in self:
+            l[func(i, self[i])] = self[i]
+        return Dic(l)
+
+    def filter(self, func):
+        l = {}
+        for i in self:
+            if func(i, self[i]):
+               l[i] = self[i]
+        return Dic(l)
+
+    def get_or_else(self, key, else_val):
+        if key in self:
+            return self[key]
+        else:
+            return else_val
+
+    def with_kv(self, key, value):
+        new_dic = copy.copy(self)
+        new_dic[key] = value
+        return new_dic
+
+    def with_dic(self, dic):
+        new_dic = copy.copy(self)
+        for key in dic:
+            new_dic[key] = dic[key]
+        return new_dic
+
+    @property
+    def to_list(self):
+        return List([(i, self[i]) for i in self])
+
+    def foreach(self, func):
+        for i in self:
+            func(i, self[i])
+
+class Option(object):
+    def __init__(self):
+        pass
+
+    def is_none(self):
+        pass
+
+    def flat_map(self, func):
+        pass
+
+    def map(self, func):
+        pass
+
+    def get(self):
+        pass
+
+    def get_or_else(self, other):
+        pass
+
+    def get_or_else_lazy(self, func):
+        pass
+
+
+class Nothing(Option):
+    def __init__(self):
+        super(Nothing, self).__init__()
+
+    def is_none(self):
+        return True
+
+    def flat_map(self, func):
+        return self
+
+    def map(self, func):
+        return self
+
+    def get(self):
+        raise AttributeError("get called on option's None")
+
+    def get_or_else(self, other):
+        return other
+
+    def get_or_else_lazy(self, func):
+        return func()
+
+class Some(Option):
+    def __init__(self, value):
+        super(Some, self).__init__()
+        self.value = value
+
+    def is_none(self):
+        return False
+
+    def flat_map(self, func):
+        return func(self.value)
+
+    def map(self, func):
+        return Some(func(self.value))
+
+    def get(self):
+        return self.value
+
+    def get_or_else(self, func):
+        return self.value
+
+    def get_or_else_lazy(self, func):
+        return self.value
+
+class Try(object):
+    def __init__(self):
+        pass
+
+    def is_failure(self):
+        pass
+
+    def is_success(self):
+        pass
+
+    def flat_map(self, func):
+        pass
+
+    def map(self, func):
+        pass
+
+    def get(self):
+        pass
+
+    def get_or_else(self, func):
+        pass
+
+class Failure(Try):
+    def __init__(self, exception):
+        super(Failure, self).__init__()
+        self.exception = exception
+
+    def is_failure(self):
+        return True
+
+    def is_success(self):
+        return False
+
+    def flat_map(self, func):
+        return self
+
+    def map(self, func):
+        return self
+
+    def result(self):
+        raise AttributeError("result called on Try's Failure")
+
+    def get_or_else(self, other):
+        return other
+
+    def to_list(self):
+        return List()
+
+class Success(Try):
+    def __init__(self, value):
+        super(Success, self).__init__()
+        self.value = value
+
+    def is_failure(self):
+        return False
+
+    def is_success(self):
+        return True
+
+    def flat_map(self, func):
+        try:
+            return func(self.value)
+        except Exception as e:
+            return Failure(e)
+
+    def map(self, func):
+        try:
+            return Success(func(self.value))
+        except Exception as e:
+            return Failure(e)
+
+    def result(self):
+        raise AttributeError("result called on Try's Failure")
+
+    def get_or_else(self, other):
+        return self.value
+
+    def to_list(self):
+        return List(self.value)
+
+        
---
+++ test_mc.py
@@ -0,0 +1,67 @@
+from mc import *
+from nose.tools import eq_
+
+
+def test_list_map():
+    eq_(List([1, 2, 3]).map(lambda x: x * 2), [2,4,6])
+
+
+def test_list_filter():
+    eq_(List([1, 2, 3]).filter(lambda x: x < 2), [1])
+
+
+def test_list_fold():
+    eq_(List([1, 2, 3]).fold(lambda x, y: x * y, 1), 6)
+
+
+def test_list_group_by():
+    eq_(
+        List([1, 2, 3, 4, 5, 6]).group_by(lambda x: x % 2),
+        {1: [1,3,5], 0: [2, 4, 6]}
+    )
+
+
+def test_list_mk_string():
+    eq_( List([5, 6, 7]).mk_string("_", "<", ">"), "<5_6_7>")
+
+
+def test_list_to_dict():
+    eq_( List([(5, 6), (7, 8)]).to_dict(), {5: 6, 7: 8})
+
+def test_list_to_set():
+    eq_(List([5,6,7]).to_set().to_list(),List([5,6,7]) )
+
+def test_set_map():
+    eq_(Set([5,6,7]).map(lambda x: x % 2), Set([1,0]) )
+
+def test_set_filter():
+    eq_(Set([5,6,7]).filter(lambda x: x % 2==0), Set([6]) )
+
+def test_dict_map_keys():
+    eq_(Dic({1: 5, 2: 6}).map_keys(lambda x,y: x * y), {5: 5, 12: 6})
+
+
+def test_dict_map_vals():
+    eq_(Dic({1: 5, 2: 6}).map_vals(lambda x,y: x + y), {1: 6, 2: 8})
+
+
+def test_dict_filter():
+    eq_(
+        Dic({1: 5, 2: 6, 4: 4}).filter(lambda x,y: y < 6 and x < 2),
+        {1: 5}
+    )
+
+def test_get_or_else():
+    dictionary = Dic({1: 2, 3: 4})
+    eq_(dictionary.get_or_else(1, 0), 2)
+    eq_(dictionary.get_or_else(2, 10), 10)
+
+def test_dic_with_kv():
+    dictionary = Dic({1: 2, 3: 4})
+    eq_(dictionary.with_kv(4,5), Dic({1:2, 3:4, 4:5}))
+    eq_(dictionary, Dic({1:2, 3:4}))
+
+def test_dic_with_dic():
+    dictionary = Dic({1: 2, 3: 4})
+    eq_(dictionary.with_dic({4:5, 6:7}), Dic({1:2, 3:4, 4:5, 6:7}))
+    eq_(dictionary, Dic({1:2, 3:4}))
